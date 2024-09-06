45398d300eedd272c2ac27efa1f61c84b9adb69e
Jeffrey Wu <wu19930628@gmail.com>
2024-09-06 15:17:08 +0800

    feature: create the enrtypage to choose identity

--- app.py
+++ app.py
@@ -1,47 +1,55 @@
 import streamlit as st
+import streamlit_authenticator as stauth
 import pymysql
 
 pymysql.install_as_MySQLdb()
 st.set_page_config(layout="wide")
+
+
 if "role" not in st.session_state:
     st.session_state.role = None
 
 ROLES = [None, "Admin", "User"]
 
 
-def login():
-    st.header("Log in")
+def choose_role():
+    st.header("Choose the role")
     role = st.selectbox("Choose role", ROLES)
-    if st.button("Login"):
+    if st.button("Submit"):
         st.session_state.role = role
         st.rerun()
 
 
-def logout():
+def back():
     st.session_state.role = None
     st.rerun()
 
 
-logout_page = st.Page(logout, title="Logout", icon=":material/logout:")
+back_page = st.Page(back, title="Back", icon=":material/logout:")
 settings = st.Page("settings.py", title="Settings", icon=":material/settings:")
-
+user_login = st.Page("user/user_login.py", title="User Login", icon="🅰️", default=(st.session_state.role == "User"))
 user_index = st.Page(
     "user/user_index.py",
     title="User Index",
     icon=":material/person:",
-    default=(st.session_state.role == "User"),
+    # default=(st.session_state.role == "User"),
 )
 user_updte = st.Page("user/user_update.py", title="User Update", icon="👤")
 user_chatbot = st.Page("user/user_chatbot.py", title="User Chatbot", icon="🤨")
 user_langchain = st.Page(
     "user/user_langchain.py", title="Build an LLM app using LangChain", icon="🤖"
 )
-
+admin_login = st.Page(
+    "admin/admin_login.py",
+    title="Admin Login",
+    icon="🅱️",
+    default=(st.session_state.role == "Admin"),
+)
 admin_page = st.Page(
     "admin/admin_index.py",
     title="Admin Index",
     icon=":material/person_add:",
-    default=(st.session_state.role == "Admin"),
+    # default=(st.session_state.role == "Admin"),
 )
 admin_page2 = st.Page("admin/admin_page2.py", title="Tooltips demo")
 admin_page3 = st.Page("admin/admin_page3.py", title="Tab demo")
@@ -71,24 +79,12 @@ admin_page11 = st.Page(
     "admin/admin_page11.py",
     title="Create a search engine with Streamlit and Google Sheets",
 )
-account_pages = [logout_page, settings]
-user_pages = [user_index, user_updte, user_chatbot, user_langchain]
-admin_pages = [
-    admin_page,
-    admin_page2,
-    admin_page3,
-    admin_page4,
-    admin_page5,
-    admin_page6,
-    admin_page7,
-    admin_page8,
-    admin_page9,
-    admin_page10,
-    admin_page11,
-]
+account_pages = [back_page, settings]
+user_pages = [user_login]
+admin_pages = [admin_login]
 
 
-st.title("Request manager")
+st.title("Streamlit Demo")
 st.divider()
 page_dict = {}
 if st.session_state.role in ["User", "Admin"]:
@@ -99,6 +95,6 @@ if st.session_state.role == "Admin":
 if len(page_dict) > 0:
     pg = st.navigation({"Account": account_pages} | page_dict)
 else:
-    pg = st.navigation([st.Page(login)])
+    pg = st.navigation([st.Page(choose_role)])
 
 pg.run()
---
+++ user/user_login.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+
+role = st.session_state.role
+
+
+st.write("User Login Page")
